fdc3d4f7c40f043c3549f65d8c788e3112cc285e
fritzgt <tuanis@live.com>
2019-12-09 14:43:18 +0200

    completed the API call and now we are getting the data back

--- ByteCoin/Controller/ViewController.swift
+++ ByteCoin/Controller/ViewController.swift
@@ -8,13 +8,52 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+//Adding the UIPickerViewDataSource protocol for the currency picker
+//Adding UIPickerViewDelegate to watch for choice option
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
 
+    //IBOutlet
+    @IBOutlet weak var bitcoinLabel: UILabel!
+    @IBOutlet weak var currencyLabel: UILabel!
+    @IBOutlet weak var currencyPicker: UIPickerView!
+    
+    //Creating instance of CoinManager
+    let coinManager = CoinManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //set as data source for currency picker to display options
+        currencyPicker.dataSource = self
+        
+        //set as delegate for currency picker activity
+        currencyPicker.delegate = self
     }
 
-
+    //1st method for the UIPickerViewDataSource protocol for columns
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        //this is how many columns to return in the picker
+        //Each calum acts as a separate picker
+        return 1
+    }
+    
+    //2nd method for the UIPickerViewDataSource protocol to create rows
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        //Instead of hard couding how many currency options we have
+        //to create the correct ammount of rows we simply count it as shuch:
+        return coinManager.currencyArray.count
+    }
+    
+    //1st for UIPickerViewDelegate to actually render the values in the array
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return coinManager.currencyArray[row]
+    }
+    
+    //2nd This method checks which option is currently selected in the picker
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let selectedCurrency = coinManager.currencyArray[row]
+        coinManager.getCoinPrice(for: selectedCurrency)
+    }
 }
-
--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -13,4 +13,53 @@ struct CoinManager {
     let baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC"
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
     
+    
+    //Get current selected currency comming from didSelectRow pickerView func
+    //Then we call the performRequest method to pass the url
+    func getCoinPrice(for currency: String)  {
+        
+        //URL format per docs https://apiv2.bitcoinaverage.com/#price-symbols-ticker
+        let urlString = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC\(currency)"
+        
+        //Invoke performRequest to pass URL
+        performRequest(with: urlString)
+    }
+    
+    //Creating the URL to contact API
+    func performRequest(with urlString: String) {
+        
+        //1.Using the URL method and since is an optional we use if to unwrap it
+        //This will be pass to the session task below
+        if let url = URL(string: urlString){
+            
+            //2.Creating session
+            //The URLSession class and related classes provide an API for downloading data from and uploading data to endpoints indicated by URLs.
+            let session = URLSession(configuration: .default)
+            
+            //3.Now we access the dataTask property of session
+            //To give the session a task to complete
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    //self.delegate?.didFailWithError(error: error!)
+                    print(error!)
+                    return
+                }
+                
+                if let safeData = data {
+                    //parseJSON is a method created below this FUNC
+                    //                    if let currencyValue =  self.parseJSON(safeData){
+                    //                        self.delegate?.didUpdateWeather(self, currencyValue:currencyValue)
+                    //                        return
+                    //                    }
+                    
+                    let backToString = String(data: safeData, encoding: String.Encoding.utf8) as String?
+                    
+                    print(backToString!)
+                }
+            }
+            //4.Start the task, is call resume
+            task.resume()
+        }
+    }
+    
 }
